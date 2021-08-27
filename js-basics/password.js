55dcf469122fbbbf1150724c9b0c2ecafd598004
Zachary Moh <88344516+dedzach@users.noreply.github.com>
2021-08-27 10:54:54 -0500

    Add files via upload

---
+++ js-basics/password.js
@@ -0,0 +1,10 @@
+console.log ('Welcome to the password validation!')
+function CheckPassword( password ) {
+  let input = ('pokemoniscool123')
+  if (password.value.(input)>[10]) {
+    console.log('Congratulations!')
+  } else {
+    console.log('Your password fails to meet the 10 character minimum. Please try again.')
+  }
+}
+
